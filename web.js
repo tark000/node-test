df3ecf888a2cb42b03085fc0039602468b611d26
taras <tark000@ukr.net>
2016-09-15 09:31:04 +0300

    express

--- web.js
+++ web.js
@@ -17,39 +17,40 @@
 // console.log("Server running at http://127.0.0.1:8000/");
 
 
-var http = require('http'),
-    fs = require('fs');
+// var http = require('http'),
+//     fs = require('fs');
 
+// var port = process.env.PORT || 8000;
+// fs.readFile('./frontend/index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(port);
+// });
+
+var express = require("express");
+var app     = express();
+var path    = require("path");
 var port = process.env.PORT || 8000;
-fs.readFile('./frontend/index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port);
+app.use(express.static(__dirname));
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/frontend/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-// var express = require("express");
-// var app     = express();
-// var path    = require("path");
-// var port = process.env.PORT || 8000;
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
 
-// app.get('/',function(req,res){
-//   res.sendFile(path.join(__dirname+'/frontend/index.html'));
-//   //__dirname : It will resolve to your project folder.
-// });
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
 
-// app.get('/about',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
-// });
 
-// app.get('/sitemap',function(req,res){
-//   res.sendFile(path.join(__dirname+'/sitemap.html'));
-// });
+app.listen(port);
 
-// app.listen(port);
-
-// console.log("Running at Port 3000");
+console.log("Running at Port 8000");
