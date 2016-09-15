1db4bd1802d7ffd220c0462d3c63ea9995cbaad5
taras <tark000@ukr.net>
2016-09-15 08:56:23 +0300

    test2

--- web.js
+++ web.js
@@ -4,13 +4,14 @@ var http = require('http');
 // var fs = require('fs');
 // console.log(fs);
 // Configure our HTTP server to respond with Hello World to all requests.
+var port = process.env.PORT || 8000;
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World web.js\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(port);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
